f04a6a4c1a1c90dc9f964a7094396a3fba5ed424
Carl John <135079894+CjsPortfolio@users.noreply.github.com>
2024-05-19 21:33:15 +0800

    Update sendnoti.js

--- script/sendnoti.js
+++ script/sendnoti.js
@@ -21,7 +21,7 @@ module.exports.run = async function ({ api, event, args, admin }) {
 	async function sendMessage(thread) {
 		try {
 			await api.sendMessage(
-`---------------\n『 𝐍𝐎𝐓𝐈𝐅𝐈𝐂𝐀𝐓𝐈𝐎𝐍 』\n\n ----------------\n𝒂𝒅𝒎𝒊𝒏:「${custom}」\n _________________________`,
+`\n—————————————\n『 𝐍𝐎𝐓𝐈𝐅𝐈𝐂𝐀𝐓𝐈𝐎𝐍 』\n—————————————\n𝒂𝒅𝒎𝒊𝒏:「${custom}」\n —————————————`,
 				thread.threadID
 			);
 			sentCount++;
