673cf6591e156d1043dcc787712df8024a14104c
Carl John <135079894+CjsPortfolio@users.noreply.github.com>
2024-05-19 21:31:26 +0800

    Update prefix.js

--- script/prefix.js
+++ script/prefix.js
@@ -34,26 +34,51 @@ module.exports.run = function ({ api, event, prefix, admin }) {
     }
 
     // Sending the message along with the attachment
-    api.sendMessage(
-        {
-            body: `Yo, my prefix is [ 𓆩 ${prefix} 𓆪 ]\n\n𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:\n➥ ${prefix}help [number of page] -> see commands\n➥ ${prefix}sim [message] -> talk to bot\n➥ ${prefix}callad [message] -> report any problem encountered\n➥ ${prefix}help [command] -> information and usage of command\n\nHave fun using it, enjoy! ❤️\nBot Developer: https://www.facebook.com/Churchill.Dev4100`,
-            attachment: fs.createReadStream(__dirname + "/cache2/prefix.jpeg")
-        },
-        threadID,
-        (err, messageInfo) => {
-            if (err) return console.error(err);
-
-            const voiceFile = fs.readFileSync(__dirname + "/cache2/prefix.jpeg");
-            api.sendMessage(
-                {
-                    attachment: voiceFile,
-                    type: "audio",
-                    body: "Hey, listen to my prefix information!",
-                },
-                threadID,
-                () => {}
-            );
-            api.setMessageReaction("🚀", messageInfo.messageID, (err) => {}, true);
+    const attachmentPath = __dirname + "/cache2/prefix.jpeg";
+    fs.exists(attachmentPath, (exists) => {
+        if (!exists) {
+            console.error("Attachment not found at path:", attachmentPath);
+            return;
         }
-    );
+
+        const attachmentStream = fs.createReadStream(attachmentPath);
+        api.sendMessage(
+            {
+                body: `Yo, my prefix is [ 𓆩 ${prefix} 𓆪 ]\n\n𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:\n➥ ${prefix}help [number of page] -> see commands\n➥ ${prefix}sim [message] -> talk to bot\n➥ ${prefix}callad [message] -> report any problem encountered\n➥ ${prefix}help [command] -> information and usage of command\n\nHave fun using it, enjoy! ❤️\nBot Developer: https://www.facebook.com/Churchill.Dev4100`,
+                attachment: attachmentStream
+            },
+            threadID,
+            (err, messageInfo) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+
+                const voiceFilePath = __dirname + "/cache2/prefix.mp3";
+                fs.exists(voiceFilePath, (exists) => {
+                    if (!exists) {
+                        console.error("Voice file not found at path:", voiceFilePath);
+                        return;
+                    }
+
+                    const voiceFile = fs.createReadStream(voiceFilePath);
+                    api.sendMessage(
+                        {
+                            body: "Hey, listen to my prefix information!",
+                            attachment: voiceFile,
+                            type: "audio",
+                        },
+                        threadID,
+                        () => {
+                            api.setMessageReaction("🚀", messageInfo.messageID, (err) => {
+                                if (err) {
+                                    console.error(err);
+                                }
+                            }, true);
+                        }
+                    );
+                });
+            }
+        );
+    });
 };
